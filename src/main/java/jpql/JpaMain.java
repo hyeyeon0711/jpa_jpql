975ff9b6aad1f6b32858d1b0d7c72bee2cbf5d21
khy <daldal0711@naver.com>
2023-06-16 15:18:25 +0900

    [feat] JPQL Function

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,21 +13,58 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            List<MemberDTO> result = em.createQuery("SELECT new jpql.MemberDTO(m.username, m.age) FROM Member m", MemberDTO.class).getResultList();
 
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("memberDTO(username) = " + memberDTO.getUsername());
-            System.out.println("memberDTO(age)      = " + memberDTO.getAge());
+            String query = "select function('group_concat', m.username) FROM Member m";
+
+            List<String> result = em.createQuery(query, String.class).getResultList();
+
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }
+            /*String query =
+                    "select " +
+                        "case when m.age <= 10 then '학생요금' " +
+                        "     when m.age >= 60 then '경로요금' " +
+                        "     else '일반요금' " +
+                        "end " +
+                    "from Member m ";
+            
+            List<String> result = em.createQuery(query, String.class).getResultList();
+
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }*/
 
             tx.commit();
+//            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+//            List<Member> result = em.createQuery(query, Member.class)
+//                    .getResultList();
+//
+//            System.out.println("result.size = " + result.size());
+
+//            System.out.println("result.size = " + result.size());
+//
+//            for (Member member1 : result) {
+//                System.out.println("member1 = " + member1);
+//            }
+
+
+//            List<MemberDTO> result = em.createQuery("SELECT new jpql.MemberDTO(m.username, m.age) FROM Member m", MemberDTO.class).getResultList();
+//
+//            MemberDTO memberDTO = result.get(0);
+//            System.out.println("memberDTO(username) = " + memberDTO.getUsername());
+//            System.out.println("memberDTO(age)      = " + memberDTO.getAge());
         } catch (Exception e) {
             e.printStackTrace();
             tx.rollback();
--- src/main/java/jpql/MemberDTO.java
+++ src/main/java/jpql/MemberDTO.java
@@ -25,4 +25,6 @@ public class MemberDTO {
     public void setAge(int age) {
         this.age = age;
     }
+
+
 }
